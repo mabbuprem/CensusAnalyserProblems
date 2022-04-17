203ca16a6ad7a44de7a3799c094f0eb17278cb87
mabbuprem <mabbupremkumar@gmail.com>
2022-04-17 08:09:18 +0530

    Add project files.

---
+++ CensusAnalyserProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Census Analyser");
+        }
+    }
+}
